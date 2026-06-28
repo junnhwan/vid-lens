0f427ef7cad9506b8ddc165d78cb4890049d655d
Junhuan Zheng <3373484735@qq.com>
2026-06-28 11:15:38 +0800

    chore: ignore local source notes

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -10,10 +10,10 @@ import (
 // CORS 跨域中间件
 func CORS() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:  []string{"http://localhost:8080", "http://localhost:5173", "http://127.0.0.1:5173"},
-		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:  []string{"Origin", "Content-Type", "Authorization"},
-		ExposeHeaders: []string{"Content-Length"},
-		MaxAge:        12 * time.Hour,
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:    []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:   []string{"Content-Length"},
+		MaxAge:          12 * time.Hour,
 	})
 }
